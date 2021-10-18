9c1b07be295d197d446e475973d64082cb1c20c6
vincent <vincent.biasin@outlook.fr>
2021-10-18 21:45:19 +0200

    premier test config externalisee

--- BibliothequeSpringCloudConfig/src/main/java/com/ocp7bibliotheque/bibliothequespringcloudconfig/BibliothequeSpringCloudConfigApplication.java
+++ BibliothequeSpringCloudConfig/src/main/java/com/ocp7bibliotheque/bibliothequespringcloudconfig/BibliothequeSpringCloudConfigApplication.java
@@ -2,6 +2,7 @@ package com.ocp7bibliotheque.bibliothequespringcloudconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
