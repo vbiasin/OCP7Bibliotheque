da7c76e1b7ca19ef9d4e84da541239eac6eec4bb
vincent <vincent.biasin@outlook.fr>
2021-10-27 19:12:35 +0200

    Deuxième tentative externalisation des configs

--- BibliothequeEureka/src/main/java/com/ocp7bibliotheque/bibliothequeeureka/BibliothequeEurekaApplication.java
+++ BibliothequeEureka/src/main/java/com/ocp7bibliotheque/bibliothequeeureka/BibliothequeEurekaApplication.java
@@ -2,8 +2,10 @@ package com.ocp7bibliotheque.bibliothequeeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class BibliothequeEurekaApplication {
 
     public static void main(String[] args) {
