1ed04d4262916cfaae5b8d1aad8814b120b5fc08
vincent <vincent.biasin@outlook.fr>
2021-11-09 21:23:33 +0100

    ajout module bibliothequeBook (avec module)

---
+++ BibliothequeBook/src/main/java/com/ocp7bibliotheque/bibliothequebook/BibliothequeBookApplication.java
@@ -0,0 +1,13 @@
+package com.ocp7bibliotheque.bibliothequebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliothequeBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliothequeBookApplication.class, args);
+    }
+
+}
---
+++ BibliothequeBook/src/test/java/com/ocp7bibliotheque/bibliothequebook/BibliothequeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocp7bibliotheque.bibliothequebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothequeBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
