cf375032e8687f5131fb3c2819966882052bed46
vincent <vincent.biasin@outlook.fr>
2022-02-16 17:44:56 +0100

    modif employee, userAccount et suppression Member

--- BibliothequeWeb/src/main/java/com/ocp7bibliotheque/bibliothequeweb/DAO/MemberRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ocp7bibliotheque.bibliothequeweb.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,Integer> {
-}
