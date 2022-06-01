d29445d0ae83093d952bd02200a50fe54142974d
vincent <vincent.biasin@outlook.fr>
2022-06-01 19:14:41 +0200

    uniformisation version Java (Java 15)

--- BibliothequeBatchMail/src/main/java/com/ocp7bibliotheque/bibliothequebatchmail/Mail/MyConstants.java
+++ BibliothequeBatchMail/src/main/java/com/ocp7bibliotheque/bibliothequebatchmail/Mail/MyConstants.java
@@ -2,7 +2,7 @@ package com.ocp7bibliotheque.bibliothequebatchmail.Mail;
 
 public class MyConstants {
 
-    public static final String MY_EMAIL = "OCP7OrbisUnuml@gmail.com";
+    public static final String MY_EMAIL = "OCP7OrbisUnum@gmail.com";
     public static final String MY_PASSWORD = "*67Str@sb0urg";
 
 }
--- BibliothequeBatchMail/src/main/java/com/ocp7bibliotheque/bibliothequebatchmail/Service/MailAlert.java
+++ BibliothequeBatchMail/src/main/java/com/ocp7bibliotheque/bibliothequebatchmail/Service/MailAlert.java
@@ -3,10 +3,13 @@ package com.ocp7bibliotheque.bibliothequebatchmail.Service;
 import com.ocp7bibliotheque.bibliothequebatchmail.DAO.LendingRepository;
 
 import com.ocp7bibliotheque.bibliothequebatchmail.Entites.Lending;
+import com.ocp7bibliotheque.bibliothequebatchmail.Mail.MyConstants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -14,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Properties;
 
 @Configuration
 @EnableScheduling
@@ -27,6 +31,24 @@ public class MailAlert {
     @Autowired
     LendingRepository lendingRepository;
 
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(MyConstants.MY_EMAIL);
+        mailSender.setPassword(MyConstants.MY_PASSWORD);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
     @Scheduled(fixedRate = 600000)
     public void mailDiffusor(){
         List<Lending> lendings = lendingRepository.findAll() ;
@@ -37,10 +59,10 @@ public class MailAlert {
                 message.setTo(lending.getUserAccount().getMail());
                 message.setSubject("Prêt en retard");
                 message.setText("Bonjour, votre prêt "+lending.getBook().getTitle()+" effectué le " +lending.getStartDate()+" à la librairie: "
-                        +lending.getBook().getLibrary().getName() + " est arrivé à échéance. Si ce n'est pas déjà le cas vous pouvez le prolonger dans votre espace personnelle" +
+                        +lending.getBook().getLibrary().getName() + " est arrivé à échéance. Si ce n'est pas déjà le cas vous pouvez le prolonger dans votre espace personnel. " +
                         "Nous vous prions de retourner l'ouvrage dans les plus brefs délais afin de régulariser votre situation.");
                 // Send Message!
-                this.emailSender.send(message);
+            this.emailSender.send(message);
             }
         }
     }
